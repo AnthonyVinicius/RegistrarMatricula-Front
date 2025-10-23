d254b3524085655fd9e2aceb645e7891fb9087f5
Anthony Vinicius <91100881+AnthonyVinicius@users.noreply.github.com>
2025-10-23 10:10:19 -0300

    Fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
+import CourseRegisterView from "../view/CourseRegisterView.vue";
 import UserRegisterView from "../view/UserRegisterView.vue";
-import CourseRegisterView from "../view/CourseRegisterView.vue"
 
 const router = createRouter({
   history: createWebHistory(),
